bf845871dfb74418c671a7bc2eaa67f6846d964b
Molten <139630170+tem-Italy@users.noreply.github.com>
2024-11-26 18:25:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function unlockLink() {
     const unlockButton = document.querySelector('.unlockbutton');
 
     if(inputPassword == decryptedPassword) {
-        window.location.href = "https://docs.google.com/spreadsheets/u/0/d/" + linkId + "/edit";
+        document.location.href = "https://docs.google.com/spreadsheets/u/0/d/" + linkId + "/edit";
     } else {
         unlockButton.innerHTML = "Wrong Password!";
         setTimeout(() => {
